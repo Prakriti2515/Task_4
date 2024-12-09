a4ca88142be946548dc6b9bbe8172e19f26a3461
Prakriti <anukriti.1911@gmail.com>
2024-12-09 22:50:31 +0530

    Vehicle detail schema updated

--- rides/model/vehicle.js
+++ rides/model/vehicle.js
@@ -2,20 +2,23 @@ const User = require('../../authentication/src/models/Schema');
 const mongoose = require('mongoose');
 const vehicleSchema = new mongoose.Schema({
     driver: {
-        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
     }, // link to the driver
     vehicleType: {
         type: String
     },
-    dateTime: {
-        type: Date
+    vehicleModel: {
+        type: String
     },
-    availableSeats: {
-        type: Number, 
-        default: 0 
-    } // Seats available for passengers
-  });
+    number_plate: {
+        type: String, 
+    },
+    yearOfPurchase: {
+        type: String
+    }
+});
 
-  const Vehicle = mongoose.model('Vehicle', vehicleSchema);
-  module.exports = Vehicle;
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+module.exports = Vehicle;
   
